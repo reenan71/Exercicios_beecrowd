d65b5be7420ab571b86a26cab839ddfca12c1f5c
Robson Renan <101532954+reenan71@users.noreply.github.com>
2025-02-16 19:31:50 -0300

    Finalizando exercicio 1073

---
+++ URI_1073/Program.cs
@@ -0,0 +1,13 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int N = int.Parse(Console.ReadLine());
+        for (int index = 2; index <= N; index += 2)
+        {
+            Console.WriteLine($"{index}^2 = {index* index}");
+        }
+    }
+}
