2ba80514cc476f05a59072e04eb0bde163392189
Robson Renan <robsonrenan45@gmail.com>
2025-02-16 16:06:27 -0300

    Finalizando exercicio 1070

---
+++ URI_1070/Program.cs
@@ -0,0 +1,19 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+      
+      int X = int.Parse(Console.ReadLine());
+
+      if(X % 2 == 0)
+        {
+          X++;
+        }
+      for(int index = 0; index < 6; index++)
+      {
+          Console.WriteLine(X + 2 * index);
+      }
+    }
+
+}
