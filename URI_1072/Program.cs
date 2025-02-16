52d0fb8bf40b8b0153e3a37f85c0b84d05e1bee3
Robson Renan <101532954+reenan71@users.noreply.github.com>
2025-02-16 18:43:00 -0300

    Finalizando exercicio 1072

---
+++ URI_1072/Program.cs
@@ -0,0 +1,30 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int N = int.Parse(Console.ReadLine());
+
+        int inCount = 0;
+        int outCount = 0;
+
+        for(int index = 0; index < N; index++)
+        {
+            int X = int.Parse(Console.ReadLine());
+
+            if(X >= 10 && X <= 20)
+            {
+                inCount++;
+            }
+            else
+            {
+                outCount++;
+            }
+        }
+        Console.WriteLine(inCount + " in");
+        Console.WriteLine(outCount + " out");
+
+    }
+
+}
