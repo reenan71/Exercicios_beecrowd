64fe74bdd40a24af82bd512644cb74e178983618
Robson Renan <robsonrenan45@gmail.com>
2025-02-20 11:09:48 -0300

    Finalizando exercicio URI_1078

---
+++ URI_1078/Program.cs
@@ -0,0 +1,20 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+      int N = int.Parse(Console.ReadLine());
+
+      if (2 < N && N < 1000)
+        {
+          for (int i = 1; i <= 10; i++)
+          {
+            Console.WriteLine($"{i} X {N} = {i * N}");
+          }
+        }
+      
+
+    }
+
+}
