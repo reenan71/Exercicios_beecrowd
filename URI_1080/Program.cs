90b7ba2b862081c97f6f620db3f5cd963340466a
Robson Renan <robsonrenan45@gmail.com>
2025-02-21 11:24:09 -0300

    Finalizando exercicio 1080

---
+++ URI_1080/Program.cs
@@ -0,0 +1,28 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int [] valores = new int[100];
+        for(int i = 0; i < 100; i++)
+        {
+          valores[i] = int.Parse(Console.ReadLine());
+        }
+
+        int MaiorValor = valores[0];
+        int posicao = 1;
+
+        for (int i = 1; i < 100; i++)
+        {
+          if(valores[i] > MaiorValor)
+          {
+            MaiorValor = valores[i];
+            posicao = i + 1;
+          }
+        }
+        Console.WriteLine(MaiorValor);
+        Console.WriteLine(posicao);
+    }
+
+}
