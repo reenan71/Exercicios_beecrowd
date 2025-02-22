3576a44ada58685afc3a936785da53580f01bc75
Robson Renan <101532954+reenan71@users.noreply.github.com>
2025-02-22 11:55:51 -0300

    Finalizando exercicios 1095/1096

---
+++ URI_1095/Program.cs
@@ -0,0 +1,19 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int i = 1;
+        int j = 60;
+
+        while (j >= 0)
+        {
+            Console.WriteLine("I=" + i + " J=" + j);
+            i += 3;
+            j -= 5;
+        }
+
+    }
+
+}
