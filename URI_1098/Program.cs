4f02e47a42f1931424ed9ecc231848589375a0e5
Robson Renan <101532954+reenan71@users.noreply.github.com>
2025-02-22 19:45:42 -0300

    Finalizando exercicio 1098

---
+++ URI_1098/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        float i, j;
+
+        for (i = 0f; i <= 2.1f; i += 0.2f)
+        {
+            for (j = 1.0f; j <= 3.0f; j++)
+            {
+                if (i > 0 && i < 1 || i > 1 && i < 2)
+                {
+                    Console.WriteLine("I={0} J={1}", i.ToString("0.0", CultureInfo.InvariantCulture), (j + i).ToString("0.0", CultureInfo.InvariantCulture));
+                }
+                else
+                {
+                    Console.WriteLine("I={0} J={1}", i.ToString("0", CultureInfo.InvariantCulture), (j + i).ToString("0", CultureInfo.InvariantCulture));
+                }
+            }
+        }
+    }
+}
