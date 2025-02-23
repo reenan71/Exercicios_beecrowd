bdd7c314a8cebbe573cbaa0ab69d529e36f0ba22
Robson Renan <101532954+reenan71@users.noreply.github.com>
2025-02-23 11:01:37 -0300

    Finalizando exercicio 1099

---
+++ URI_1099/Program.cs
@@ -0,0 +1,31 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int N = int.Parse(Console.ReadLine());
+        
+        for(int i = 0; i < N; i++)
+        {
+            string [] input = Console.ReadLine().Split(' ');
+            int X = int.Parse(input[0]);
+            int Y = int.Parse(input[1]);
+            
+            int minimo = Math.Min(X, Y);
+            int maximo = Math.Max(X, Y);
+            int soma = 0;
+            
+            for(int j = minimo + 1; j < maximo; j++)
+            {
+                if(j % 2 != 0)
+                {
+                    soma += j;
+                }
+            }
+            Console.WriteLine(soma);
+        }
+
+    }
+
+}
