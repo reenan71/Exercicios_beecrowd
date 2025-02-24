df354053409e7378c76bdf373cfdbb90cb3c9b6f
Robson Renan <101532954+reenan71@users.noreply.github.com>
2025-02-24 11:10:44 -0300

    Finalizando exercicio 1114

---
+++ URI_1114/Program.cs
@@ -0,0 +1,20 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+        while(true){
+            int senhaFixa = 2002;
+            int senha = int.Parse(Console.ReadLine());
+        
+            if(senha == senhaFixa){
+            Console.WriteLine("Acesso Permitido");
+            break;
+            }else{
+            Console.WriteLine("Senha Invalida");
+            }
+        }
+        
+    }
+
+}
