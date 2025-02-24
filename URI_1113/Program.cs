f320f6e534edb8d21107b862b2948cf98fa060f7
Robson Renan <101532954+reenan71@users.noreply.github.com>
2025-02-24 10:59:03 -0300

    Finalizando exercicio 1113

---
+++ URI_1113/Program.cs
@@ -0,0 +1,23 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+        
+        while(true){
+            string[] input = Console.ReadLine().Split(' ');
+            int X = int.Parse(input[0]);
+            int Y = int.Parse(input[1]);
+            
+            if(X == Y){
+                break;
+            }
+        
+            if(X < Y){
+            Console.WriteLine("Crescente");
+            }else{
+            Console.WriteLine("Decrescente");
+            }
+        }
+    }
+}
