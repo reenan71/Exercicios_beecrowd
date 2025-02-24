9c3497ce0e5f9a41807d8e9da0a3a14ef00698da
Robson Renan <101532954+reenan71@users.noreply.github.com>
2025-02-24 17:47:08 -0300

    Finalizando exercicio 1115

---
+++ URI_1115/Program.cs
@@ -0,0 +1,37 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        while(true){
+            string [] input = Console.ReadLine().Split(' ');
+            int X = int.Parse(input[0]);
+            int Y = int.Parse(input[1]);
+            
+            if(X == 0 || Y == 0)
+            {
+                break;
+            }
+            
+            if(X > 0 && Y > 0)
+            {
+                Console.WriteLine("primeiro");
+            }
+            if(X < 0 && Y > 0)
+            {
+                Console.WriteLine("segundo");
+            }
+            if(X < 0 && Y < 0)
+            {
+                Console.WriteLine("terceiro");
+            }
+            if(X > 0 && Y < 0)
+            {
+                Console.WriteLine("quarto");
+            }
+        }
+
+    }
+
+}
