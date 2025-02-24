03db30de2c973c2d9b24ad1504523616221014ae
Robson Renan <101532954+reenan71@users.noreply.github.com>
2025-02-24 18:07:03 -0300

    Finalizando exercicio 1116

---
+++ URI_1116/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int N = int.Parse(Console.ReadLine());
+        for (int i = 0; i < N; i++)
+        {
+            string[] valores = Console.ReadLine().Split();
+            double X = double.Parse(valores[0]);
+            double Y = double.Parse(valores[1]);
+            if (Y == 0)
+            {
+                Console.WriteLine("divisao impossivel");
+            }
+            else
+            {
+                double divisao = X / Y;
+                Console.WriteLine(divisao.ToString("F1", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
