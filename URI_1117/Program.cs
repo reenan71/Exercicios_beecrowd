c43531df0e31856d8f6e56e4d2dca8050d41d260
Robson Renan <101532954+reenan71@users.noreply.github.com>
2025-02-24 22:02:56 -0300

    Finalizando exercicio 1117

---
+++ URI_1117/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        double soma = 0;
+        int notasValidas = 0;
+
+        while (notasValidas < 2)
+        {
+            double nota = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (nota < 0 || nota > 10)
+            {
+                Console.WriteLine("nota invalida");
+            }
+            else
+            {
+                soma += nota;
+                notasValidas++;
+            }
+        }
+
+        double media = soma / 2;
+        Console.WriteLine($"media = {media.ToString("F2", CultureInfo.InvariantCulture)}");
+    }
+}
