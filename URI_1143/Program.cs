38643f8abcea05a930cf30a64c827504e70e3916
Robson Renan <101532954+reenan71@users.noreply.github.com>
2025-02-26 21:49:25 -0300

    Finalizando exercicio 1143

---
+++ URI_1143/Program.cs
@@ -0,0 +1,17 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int N = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= N; i++)
+        {
+            int quadrado =  i * i;
+            int cubo = i * i * i;
+            Console.WriteLine(i + " " + quadrado + " " + cubo);
+        }
+
+    }
+
+}
