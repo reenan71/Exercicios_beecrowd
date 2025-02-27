fec9a85bf2749d07f4d45239da718c69643f8b01
Robson Renan <101532954+reenan71@users.noreply.github.com>
2025-02-27 20:30:00 -0300

    Finalizando exercicio 1146

---
+++ URI_1146/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class URI {
+    static void Main(string[] args) { 
+        int X = int.Parse(Console.ReadLine());
+        
+        while (X != 0) {
+            for (int i = 1; i <= X; i++) {
+                if (i == X) {
+                    Console.WriteLine(i);
+                } else {
+                    Console.Write(i + " ");
+                }
+            }
+            X = int.Parse(Console.ReadLine()); // Leitura de uma nova entrada
+        }
+    }
+}
