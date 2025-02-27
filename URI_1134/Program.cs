35bdede4f5a0cb56e532f9fe6ec2dee24dc7c4aa
Robson Renan <101532954+reenan71@users.noreply.github.com>
2025-02-26 21:22:02 -0300

    Finalizando exercicio 1134

---
+++ URI_1134/Program.cs
@@ -0,0 +1,42 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int alcool = 0;
+        int gasolina = 0;
+        int diesel = 0;
+
+        while (true)
+        {
+            int tipo = int.Parse(Console.ReadLine());
+            if (tipo == 1)
+            {
+                alcool++;
+            }
+            else if (tipo == 2)
+            {
+                gasolina++;
+            }
+            else if (tipo == 3)
+            {
+                diesel++;
+            }
+            else if (tipo == 4)
+            {
+                break;
+            }
+            else {
+                continue;
+            }
+        }
+
+        Console.WriteLine("MUITO OBRIGADO");
+        Console.WriteLine("Alcool: " + alcool); 
+        Console.WriteLine("Gasolina: " + gasolina);
+        Console.WriteLine("Diesel: " + diesel);
+
+    }
+
+}
