da056ae45a299a6e3d9cef727b900c5560d82585
Robson Renan <robsonrenan45@gmail.com>
2025-02-27 13:55:24 -0300

    Finalizando exercicio 1144

---
+++ URI_1144/Program.cs
@@ -0,0 +1,19 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int N = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= N; i++)
+        {
+            int quadrado =  i * i;
+            int quadrado2 = quadrado * i;
+            
+            Console.WriteLine(i + " " + quadrado + " " + quadrado2);
+            Console.WriteLine(i + " " + (quadrado + 1) + " " + (quadrado2 + 1));
+        }
+
+    }
+
+}
