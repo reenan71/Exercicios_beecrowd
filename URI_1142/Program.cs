0b92a2fc1815bfa18c7b685e33d57c657b709b50
Robson Renan <101532954+reenan71@users.noreply.github.com>
2025-02-26 21:41:56 -0300

    Finalizando exercicio 1142

---
+++ URI_1142/Program.cs
@@ -0,0 +1,22 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int N = int.Parse(Console.ReadLine());
+        int count = 1;
+
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write(count + " ");
+                count++;
+            }
+            Console.WriteLine("PUM");
+            count++;
+        }
+    }
+
+}
