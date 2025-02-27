237f3a8ac7ef4a9e0739fc98ba58290c26f5a263
Robson Renan <robsonrenan45@gmail.com>
2025-02-27 14:20:22 -0300

    Finalizando exercicio 1145

---
+++ URI_1145/Program.cs
@@ -0,0 +1,25 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        string[] input = Console.ReadLine().Split(' ');
+        int X = int.Parse(input[0]);
+        int Y = int.Parse(input[1]);
+        
+        for(int i = 1; i <= Y; i++)
+        {
+            if(i % X == 0)
+            {
+                Console.WriteLine(i);
+            }
+            else
+            {
+                Console.Write(i + " ");
+            }
+        }
+
+    }
+
+}
