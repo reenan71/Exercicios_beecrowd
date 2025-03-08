d20f8247ddc44d672a8642077f081c2c75ae9663
Robson Renan <101532954+reenan71@users.noreply.github.com>
2025-03-08 08:19:54 -0300

    Finalizando exercicio 1150

---
+++ URI_1150/Program.cs
@@ -0,0 +1,29 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int X = int.Parse(Console.ReadLine());
+        int Z = 0;
+
+        do
+        {
+            Z = int.Parse(Console.ReadLine());
+        }while(Z <= X);
+
+        int soma = 0;
+        int contador = 0;
+
+        for(int i = X; soma <= Z; i++)
+        {
+            soma += i;
+            contador++;
+        }
+
+        
+        Console.WriteLine(contador);
+
+    }
+
+}
