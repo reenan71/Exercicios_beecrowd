810a011e1cfeea9122e4b84bd64cc3ba1a9bc4a2
Robson Renan <101532954+reenan71@users.noreply.github.com>
2025-03-08 08:49:53 -0300

    Finalizando exercicio 1151

---
+++ URI_1151/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int N = int.Parse(Console.ReadLine());
+        int primeiro = 0;
+        int segundo = 1;
+        int serie = 0;
+        
+        for (int i = 0; i < N; i++)
+        {
+            if (i == 0)
+            {
+                Console.Write(primeiro);
+            }
+            else if (i == 1)
+            {
+                Console.Write(" " + segundo);
+            }
+            else
+            {
+                serie = primeiro + segundo;
+                Console.Write(" " + serie);
+                primeiro = segundo;
+                segundo = serie;
+            }
+        }
+
+        Console.WriteLine(); 
+    }
+
+}
