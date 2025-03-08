bbcb3f092f6b24274135354b5043268f84a7df7c
Robson Renan <101532954+reenan71@users.noreply.github.com>
2025-03-08 11:53:43 -0300

    Finalizando exercicio 1153

---
+++ URI_1153/Program.cs
@@ -0,0 +1,21 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int N = int.Parse(Console.ReadLine());
+        
+        if(N > 0 && N < 13)
+        {
+            int fatorial = 1;
+            for(int i = 1; i <= N; i++)
+            {
+                fatorial *= i;
+            }
+        
+            Console.WriteLine(fatorial);
+        }
+    }
+
+}
