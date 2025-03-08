6aea07f2e0a983bc7d22a3bd1dcdc5bfcf4f5283
Robson Renan <101532954+reenan71@users.noreply.github.com>
2025-03-08 12:18:08 -0300

    Finalizando exercicio 1154

---
+++ URI_1154/Program.cs
@@ -0,0 +1,30 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int somaCumulativa = 0;
+        int contador = 0;
+        double mediaIdade = 0;
+        
+        while(true)
+        {
+            int numerosPositivos = int.Parse(Console.ReadLine());
+            
+            if(numerosPositivos < 0)
+            {
+                break;
+            }
+            somaCumulativa += numerosPositivos;
+            contador++;
+        }
+        if(contador > 0)
+        {
+            mediaIdade =  (double)somaCumulativa / contador;
+            Console.WriteLine(mediaIdade.ToString("F2"));
+        }
+        
+    }
+
+}
