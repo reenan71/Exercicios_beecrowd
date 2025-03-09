3b88c72769b5a12803ee7a4794c814ab846dc6ac
Robson Renan <101532954+reenan71@users.noreply.github.com>
2025-03-08 22:17:03 -0300

    Finalizando exercicio 1156

---
+++ URI_1156/Program.cs
@@ -0,0 +1,19 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        double S = 1;
+        int numerador = 1;
+        int denominador = 1;
+        
+        for(numerador = 3, denominador = 2; numerador <= 39; numerador += 2, denominador *= 2)
+        {
+            S += (double)numerador / denominador;
+        }
+        
+        Console.WriteLine(S.ToString("F2"));
+    }
+
+}
