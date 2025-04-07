6a6553d10413e0fb829b88f8fdfab9a8bcf1383c
Robson Renan <101532954+reenan71@users.noreply.github.com>
2025-04-06 21:50:47 -0300

    Finalizando exercicio 1159

---
+++ URI_1159/Program.cs
@@ -0,0 +1,28 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        while(true) 
+        {
+            int X = int.Parse(Console.ReadLine());
+            if (X == 0) break;
+
+            if(X % 2 != 0) 
+            {
+                X += 1;
+            }
+
+            int soma = 0;
+            for(int i = 0; i < 5; i++) 
+            {
+                soma += X;
+                X += 2;
+            }
+
+            Console.WriteLine(soma);
+        }
+    }
+
+}
