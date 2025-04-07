bf1ac3d989e4f9d1095fb3fc73720c3adc53df98
Robson Renan <101532954+reenan71@users.noreply.github.com>
2025-04-06 21:26:02 -0300

    Finalizando exercicio 1158

---
+++ URI_1158/Program.cs
@@ -0,0 +1,28 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int N = int.Parse(Console.ReadLine());
+
+        for(int i = 0; i < N; i++){
+            string[] input = Console.ReadLine().Split(' ');
+            int X = int.Parse(input[0]);
+            int Y = int.Parse(input[1]);
+
+            if(X % 2 == 0) {
+                X += 1;
+            }
+
+            int soma = 0;
+            for(int j = 0; j < Y; j++){
+                soma += X + (j * 2);
+            }
+
+            Console.WriteLine(soma);
+        }
+
+    }
+
+}
