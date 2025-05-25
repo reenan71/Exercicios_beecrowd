d769c29257abf001ef4ae03a1ab414734dd06792
Robson Renan <101532954+reenan71@users.noreply.github.com>
2025-05-25 13:16:55 -0300

    Finalizando exercicio 1175

---
+++ URI_1175/Program.cs
@@ -0,0 +1,26 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args)
+    {
+
+        int[] numeros = new int[20];
+
+        for (int i = 0; i < 20; i++)
+        {
+            numeros[i] = int.Parse(Console.ReadLine());
+        }
+
+        Array.Reverse(numeros);
+
+        for (int i = 0; i < 20; i++)
+        {
+            Console.WriteLine($"N[{i}] = {numeros[i]}");
+        }
+        
+
+
+    }
+
+}
