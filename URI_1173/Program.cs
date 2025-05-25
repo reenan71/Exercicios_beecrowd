c8d59499c7bf231fec7c8579a28452bcf52d2e21
Robson Renan <101532954+reenan71@users.noreply.github.com>
2025-05-24 23:25:33 -0300

    Finalizando exercicio 1173

---
+++ URI_1173/Program.cs
@@ -0,0 +1,28 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args)
+    {
+
+        int[] numeros = new int[10];
+        int i = 0;
+
+        int X = int.Parse(Console.ReadLine());
+
+        numeros[0] = X;
+
+        for (i = 1; i < 10; i++)
+        {
+            numeros[i] = numeros[i - 1] * 2;
+        }
+
+        for (i = 0; i < 10; i++)
+        {
+            Console.WriteLine($"N[{i}] = {numeros[i]}");  
+        }
+
+
+    }
+
+}
