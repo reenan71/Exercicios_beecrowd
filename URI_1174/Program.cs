6b4c9f78ed1d5b83f4bb0a0880b7b635ad89407d
Robson Renan <101532954+reenan71@users.noreply.github.com>
2025-05-24 23:39:16 -0300

    Finalizando exercicio 1174

---
+++ URI_1174/Program.cs
@@ -0,0 +1,25 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args)
+    {
+
+        double[] vetor = new double[100];
+
+        for (int i = 0; i < 100; i++)
+        {
+            vetor[i] = double.Parse(Console.ReadLine());
+        }
+
+        for(int i = 0; i < 100; i++)
+        {
+            if (vetor[i] <= 10)
+            {
+                Console.WriteLine($"A[{i}] = {vetor[i]:F1}");
+            }
+        }
+
+    }
+
+}
