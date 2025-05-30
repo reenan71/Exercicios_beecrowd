f6d51f9dcccc0173d1e6040e2fc097fc9921cb4f
Robson Renan <101532954+reenan71@users.noreply.github.com>
2025-05-29 22:31:51 -0300

    #ADD Finalizando exercicio 1177

---
+++ URI_1177/Program.cs
@@ -0,0 +1,19 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int numero = int.Parse(Console.ReadLine());
+        int[] vetor = new int[1000];
+        for(int i = 0; i < 1000; i++)
+        {
+            vetor[i] = i % numero;
+        }
+    
+        for(int i = 0; i < 1000; i++)
+        {
+            Console.WriteLine($"N[{i}] = {vetor[i]}");
+        }
+    }
+}
