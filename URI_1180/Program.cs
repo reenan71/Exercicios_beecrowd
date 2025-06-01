90d99e83662f873c383ed329e8e535c1e9ddc2f6
Robson Renan <101532954+reenan71@users.noreply.github.com>
2025-06-01 18:46:14 -0300

    #ADD Finalizando exercicio 1180

---
+++ URI_1180/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine()); 
+        string[] entrada = Console.ReadLine().Split(' '); 
+
+        int[] valores = new int[N];
+        for (int i = 0; i < N; i++)
+        {
+            valores[i] = int.Parse(entrada[i]);
+        }
+
+        int menorValor = valores[0];
+        int posicaoMenor = 0;
+
+        for (int i = 1; i < N; i++)
+        {
+            if (valores[i] < menorValor)
+            {
+                menorValor = valores[i];
+                posicaoMenor = i;
+            }
+        }
+
+        Console.WriteLine($"Menor valor: {menorValor}");
+        Console.WriteLine($"Posicao: {posicaoMenor}");
+    }
+}
