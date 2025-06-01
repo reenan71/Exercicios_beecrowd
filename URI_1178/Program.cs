c77343b4a9720f27fc8a8daff73775524391ef1c
Robson Renan <101532954+reenan71@users.noreply.github.com>
2025-05-31 21:15:51 -0300

    #ADD Finalizando exercicio 1178

---
+++ URI_1178/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        decimal[] N = new decimal[100];
+        decimal X = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        N[0] = X;
+
+        for (int i = 1; i < 100; i++) {
+            N[i] = N[i - 1] / 2;
+        }
+
+        for (int i = 0; i < 100; i++) {
+            decimal truncated = Math.Round(N[i] * 10000) / 10000;
+            Console.WriteLine($"N[{i}] = {truncated.ToString("F4", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
