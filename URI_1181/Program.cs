f0099e9065d819cc2657c42b18cdf154b5f7370f
Robson Renan <101532954+reenan71@users.noreply.github.com>
2025-06-01 20:35:53 -0300

    #ADD Finalizando exercicio 1181

---
+++ URI_1181/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        int L = int.Parse(Console.ReadLine()); 
+        char T = char.Parse(Console.ReadLine()); 
+
+        double[,] M = new double[12, 12];
+
+        for (int i = 0; i < 12; i++)
+        {
+            for (int j = 0; j < 12; j++)
+            {
+                M[i, j] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+        }
+
+        double soma = 0.0;
+        for (int j = 0; j < 12; j++)
+        {
+            soma += M[L, j];
+        }
+
+        if (T == 'S')
+        {
+            Console.WriteLine(soma.ToString("F1", CultureInfo.InvariantCulture));
+        }
+        else if (T == 'M')
+        {
+            double media = soma / 12.0;
+            Console.WriteLine(media.ToString("F1", CultureInfo.InvariantCulture));
+        }
+    }
+}
