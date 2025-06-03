6d3bb607004189808175ff38b985c37987106300
Robson Renan <101532954+reenan71@users.noreply.github.com>
2025-06-02 22:09:14 -0300

    #ADD  Finalizando exercicio 1183

--- URI_1183/Program.cs
+++ URI_1183/Program.cs
@@ -1,15 +1,43 @@
-﻿using System; 
+﻿using System;
+using System.Globalization;
 
-class URI {
+class Program
+{
+    static void Main()
+    {
+        char T = char.Parse(Console.ReadLine()); 
 
-    static void Main(string[] args) { 
+        double[,] M = new double[12, 12];
 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
+        for (int i = 0; i < 12; i++)
+        {
+            for (int j = 0; j < 12; j++)
+            {
+                M[i, j] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+        }
 
+        double soma = 0.0;
+        for (int j = 0; j < 12; j++)
+        {
+            for(int i = 0; i < 12;i++)
+            {
+                if(i > j)
+                {
+                    soma += M[j, i];
+                }
+            }
+            
+        }
+
+        if (T == 'S')
+        {
+            Console.WriteLine(soma.ToString("F1", CultureInfo.InvariantCulture));
+        }
+        else if (T == 'M')
+        {
+            double media = soma / 66.0;
+            Console.WriteLine(media.ToString("F1", CultureInfo.InvariantCulture));
+        }
     }
-
 }
