709c43adc5f0a511bb40bb7ff7f960124ad7f4bc
Robson Renan <101532954+reenan71@users.noreply.github.com>
2025-06-09 21:46:34 -0300

    #ADD Finalizando exercicio 1478

---
+++ URI_1478/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            int N = int.Parse(Console.ReadLine());
+            if (N == 0) 
+                break;
+
+            int[,] M = new int[N, N];
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    M[i, j] = 1 + Math.Abs(i - j);
+                }
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (j == 0)
+                        Console.Write($"{M[i, j],3}");
+                    else
+                        Console.Write($" {M[i, j],3}");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(); 
+        }
+    }
+}
